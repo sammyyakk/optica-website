4fe6a20ca32045653c2f5f2aab80bec508556646
sammyyakk <sammyyakk@gmail.com>
2026-03-25 03:25:40 +0530

    chore: deleted unused/test files

--- fix_blog_metadata.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let file = fs.readFileSync('src/app/blog/[slug]/page.tsx', 'utf8');
-
-file = file.replace(/description: post.excerpt,\n      publishedTime: post.publishedAt,/, 'description: post.excerpt,\n      url: `/blog/${slug}`,\n      publishedTime: post.publishedAt,');
-
-file = file.replace(/twitter: {\n      card: "summary_large_image",/, 'alternates: {\n      canonical: `/blog/${slug}`,\n    },\n    twitter: {\n      card: "summary_large_image",');
-
-fs.writeFileSync('src/app/blog/[slug]/page.tsx', file);
